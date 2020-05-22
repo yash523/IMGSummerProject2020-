ee304be4a9f1389040273ac566920bb7f0a11205
yash khandelwal <yashkhandelwal@yashs-MacBook-Pro.local>
2020-05-22 22:43:41 +0530

    permissions of app changed

--- DevHelper/bugtracker/permissions.py
+++ DevHelper/bugtracker/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
-    Custom permission to only allow owners of an object to edit it.
+    Custom permission to only allow owners of an object and admins to edit it.
     """
 
     def has_object_permission(self, request, view, obj):
@@ -14,3 +14,11 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
         # Write permissions are only allowed to the owner and admins
         return obj.owner == request.user or request.user.groups.filter(name = 'admins').exists()
+
+
+class IsOwnerOrTeamMembersInApp(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.owner == request.user or request.user.groups.filter(name = 'admins').exists() or request.user in obj.team_members.all()
--- DevHelper/bugtracker/views.py
+++ DevHelper/bugtracker/views.py
@@ -3,7 +3,7 @@ from bugtracker.serializers import AppSerializer, UserSerializer, BugSerializer,
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework import permissions
-from bugtracker.permissions import IsOwnerOrReadOnly
+from bugtracker.permissions import IsOwnerOrReadOnly, IsOwnerOrTeamMembersInApp
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -30,8 +30,7 @@ class AppViewSet(viewsets.ModelViewSet):
     """
     queryset = App.objects.all()
     serializer_class = AppSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrTeamMembersInApp]
 
     
     def perform_create(self, serializer):
